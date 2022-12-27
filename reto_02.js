96284608a1844c6ebca5024ee03c14262a305c7e
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-12-26 19:28:05 -0500

    reto_02 updated

--- reto_02.js
+++ reto_02.js
@@ -1,14 +1,13 @@
 function countHours(year, holidays) {
-  let extraHours = 0;
-  holidays.forEach((e) => {
-    let [month, day] = e.split("/");
-
-    let date = new Date(`${year}-${month}-${day}`);
-    console.log(date);
-    if ([1, 2, 3, 4, 5].includes(date.getDay())) {
-      extraHours = extraHours + 2;
-    }
-  });
-
-  return extraHours;
+  return (
+    holidays
+      .map((e) => {
+        let date = new Date(`${year}/${e}`);
+        return [1, 2, 3, 4, 5].includes(date.getDay());
+      })
+      .reduce((count, extraHour) => count + extraHour) * 2
+  );
 }
+const year = 2022;
+const holidays = ["01/06", "04/01", "12/25"];
+countHours(year, holidays);
