dadde11327a1a8256415723b5cb5a66b24a9758e
Bryan De La Cruz <bryan.delacruza@gmail.com>
2022-12-26 19:30:31 -0500

    reto_01 updated

--- reto_01.js
+++ reto_01.js
@@ -1,23 +1,6 @@
 function wrapping(gifts) {
-
-    const envoler = (cantCaracteres) => {
-    let text = ""
-    for (let i = 0; i < cantCaracteres; i++) {
-      text = text + "*"
-    }
-
-    return text
-  }
-
-  let newGifts = gifts.map(e => {
-    let cantLetras = e.length
-
-    let envoltura = envoler(cantLetras+2)
-
-    return envoltura + "\n*" + e + "*\n" + envoltura
-
-  })
-
-  return newGifts
+  return gifts.map((gift) => {
+    let envoltura = "*".repeat(gift.length + 2);
+    return `${envoltura}\n*${gift}*\n${envoltura}`;
+  });
 }
-
